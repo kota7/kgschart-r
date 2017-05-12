21a73f918b9886f184281c9c78b7e09bbf3b906b
kota <kmori05@gmail.com>
2017-05-13 00:29:25 +0900

    app launcer, #5, v1.0.1

---
+++ R/app-launcher.R
@@ -0,0 +1,11 @@
+#' Launch shiny app
+#'
+#' Start "KGS Rank Graph Parser" applicaiton.
+#' @param ... optional arguments for \code{\link[shiny]{runApp}}
+#' @examples
+#' kgschart_app()
+#' @export
+kgschart_app <- function(...)
+{
+  shiny::runApp(system.file('shiny/kgschart-app/', package='kgschart'), ...)
+}
